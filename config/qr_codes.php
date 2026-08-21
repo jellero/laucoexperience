9bed7c3257e17689da71d915126f1db1bd7b1236
jellero <j.ellero@gmail.com>
2026-08-21 20:59:39 +0200

    Aggiungi risorsa QR chiosco

--- config/qr_codes.php
+++ config/qr_codes.php
@@ -8,4 +8,10 @@ return [
         'entry' => '/map',
         'destination' => '/mappa',
     ],
+    'chiosco' => [
+        'label' => 'Chiosco',
+        'area' => 'Chiosco',
+        'entry' => '/chiosco',
+        'destination' => '/',
+    ],
 ];
